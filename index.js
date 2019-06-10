e2dc444abe4387c0afe77a8b321b70471b521710
sandy <swa181@sfu.ca>
2019-06-09 20:00:14 -0700

    hooked up database to add student and view

--- index.js
+++ index.js
@@ -19,33 +19,14 @@ var pool = new Pool({
   ssl: true
 });
 
-
-// pool.query('select * from users', function(error, result){
-//   // if there is an error, there will be an error object here
-//   // if successful, the result will have it there
-//   // var results = { 'results': (result.rows[0].id) ? result.rows : [] };
-//   // res.render('pages/db', results);
-//   console.log(result);
-// });
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/users', function(req, res) {
-      pool.query('select * from student;', function(error, result){
-        // if there is an error, there will be an error object here
-        // if successful, the result will have it there
-        // var results = { 'results': (result.rows[0].id) ? result.rows : [] };
-        // res.render('pages/db', results);
-        console.log(result);
-      });
-  })
   .post('/addStudentData', (req, res) => {
     // adds one student entry
-    // req.body
     const {
       stid,
       name,
@@ -58,30 +39,37 @@ express()
     } = req.body;
     
     var query = `
-      INSERT INTO Student
-      VALUES (${stid}, ${name}, ${lName}, ${weight}, ${height}, ${hairColor}, ${gpa}, ${gender});
+      INSERT INTO student
+      VALUES (${stid}, '${name}', '${lName}', ${(weight)}, ${(height)}, '${hairColor}', ${(gpa)}, '${gender}');
       `;
+
     pool.query(query, function(error, result){
       // if there is an error, there will be an error object here
       // if successful, the result will have it there
       // var results = { 'results': (result.rows[0].id) ? result.rows : [] };
       // res.render('pages/db', results);
       // req.body
-      console.log(result);
+      console.log("this is good", result);
+      console.log("this is error", error);
+      pool.query('SELECT * FROM student;', (err, renderResults) => {
+        var results = { 'results': (renderResults.rows[0].stid) ? renderResults.rows : [] };
+        res.render('pages/index', results);
+      });
     });
-    res.send(JSON.stringify({}));
+
   })
-  .get('/getAllStudentData', (req, res) => {
+  .get('/', (req, res) => {
     // returns an entire array of student data
-    pool.query('select * from users', function(error, result){
+    pool.query('SELECT * FROM student;', function(error, result){
       // if there is an error, there will be an error object here
       // if successful, the result will have it there
-      // var results = { 'results': (result.rows[0].id) ? result.rows : [] };
-      // res.render('pages/db', results);
+      var results = { 'results': (result.rows[0].stid) ? result.rows : [] };
+      res.render('pages/index', results);
       // req.body
-      console.log(result);
-      res.send(JSON.stringify({}));
+      console.log("this is the error", error);
+      console.log("this is the result", result);
+      // res.send(JSON.stringify({rows: result.rows}));
     });
   })
-  .get('/', (req, res) => res.render('pages/index'))
+  // .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
